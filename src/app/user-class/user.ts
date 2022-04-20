80a9df4097a421db4f0ba49dd6a2460eec2da372
Benie <davinci.monalissa@gmail.com>
2022-04-20 20:04:33 +0300

    Add parameters to the User class, with their data types.

--- src/app/user-class/user.ts
+++ src/app/user-class/user.ts
@@ -1,2 +1,19 @@
 export class User {
+    id: number;
+    avatar_url: string;
+    name: string;
+    size: number;
+    language: any;
+    description: string;
+    watchers_count: number;
+    forks_count: number;
+    fork: boolean;
+    html_url: string;
+    gravatar_id: any;
+    created_at: string;
+    updated_at: string;
+    public_repos: number;
+    bio: string;
+    repos_url: string;
+    login: string;
 }
