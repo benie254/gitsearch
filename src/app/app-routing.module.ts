7768ab895bb75d01b941e50a044c3d42c2e40093
Benie <davinci.monalissa@gmail.com>
2022-04-20 20:43:53 +0300

    Import the HomeComponent and UserComponent to the app-routing-modul.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
+
 const routes: Routes = [];
 
 @NgModule({
