bd3082f67334a15aa780bae37b748ff54acf0f84
Benie <davinci.monalissa@gmail.com>
2022-04-20 19:38:03 +0300

    Import the Repo & User classes and the Git and ProfileRequest services to the Home component's ts file.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Repo } from '../repo-class/repo';
+import { User } from '../user-class/user';
+import { GitService } from '../git-service/git.service';
+import { ProfileRequestService } from '../profile-request-service/profile-request.service';
 
 @Component({
   selector: 'app-home',
