8aa7e932aae70515dc13b9cef105211f180ece4b
Benie <davinci.monalissa@gmail.com>
2022-04-20 19:50:19 +0300

    Import HttpClient, Observable, User & Repo classes, and environment to the Git Service's ts.

--- src/app/git-service/git.service.ts
+++ src/app/git-service/git.service.ts
@@ -1,5 +1,12 @@
 import { Injectable } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+import { Repo } from '../repo-class/repo';
+import { User } from '../user-class/user';
+
 @Injectable({
   providedIn: 'root'
 })
