c3f3a8862726d8995b37cf0717afab4c5bf1612b
Benie <davinci.monalissa@gmail.com>
2022-04-20 19:56:38 +0300

    Import HttpClient, Observable, environment, and the User class to the ProfileRequest service.

--- src/app/profile-request-service/profile-request.service.ts
+++ src/app/profile-request-service/profile-request.service.ts
@@ -1,5 +1,11 @@
 import { Injectable } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+import { Repo } from '../repo-class/repo';
+
 @Injectable({
   providedIn: 'root'
 })
